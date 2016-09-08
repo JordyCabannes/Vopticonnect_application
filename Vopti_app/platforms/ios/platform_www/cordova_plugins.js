4865b6313dcb2deae51bdbd9f11bfd3a8523e578
JordyCabannes <cabannes@etud.insa-toulouse.fr>
2016-09-08 12:41:26 +0100

    Applicatiion for iOS device which allows video-conference between 2 or 3 people. We can: - create a room - join a room - get the iOS device camera Problem: - on iOS device: video camera is in foreground - on iOS device: impssible to get the incoming video from other participants.

---
+++ Vopti_app/platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-iosrtc.Plugin",
+        "file": "plugins/cordova-plugin-iosrtc/dist/cordova-plugin-iosrtc.js",
+        "pluginId": "cordova-plugin-iosrtc",
+        "clobbers": [
+            "cordova.plugins.iosrtc"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-iosrtc": "3.1.0",
+    "cordova-plugin-whitelist": "1.2.2"
+};
+// BOTTOM OF METADATA
+});
